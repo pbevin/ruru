a4240b7272138f775bb7172133a36b52d178b695
Pete Bevin <pete@petebevin.com>
2009-11-25 19:37:26 -0500

    Basic interpreter, minimal features

---
+++ interp/ruru.rb
@@ -0,0 +1,199 @@
+require 'rubygems'
+require 'ruby_parser'
+require 'pp'
+
+class AssertionFailure < StandardError
+end
+
+class Object
+  def assert(bool, message = 'assertion failure')
+    raise AssertionFailure.new(message) unless bool
+  end
+end
+
+
+# s(:defn,
+#  :xprod,
+#  s(:args, :a, :b),
+#  s(:scope,
+#   s(:block,
+#    s(:masgn,
+#     s(:array, s(:lasgn, :i), s(:lasgn, :prod)),
+#     s(:array, s(:lit, 0), s(:lit, 0))),
+#    s(:while,
+#     s(:call,
+#      s(:lvar, :i),
+#      :<,
+#      s(:arglist, s(:call, s(:lvar, :a), :size, s(:arglist)))),
+#     s(:block,
+#      s(:lasgn,
+#       :prod,
+#       s(:call,
+#        s(:lvar, :prod),
+#        :+,
+#        s(:arglist,
+#         s(:call,
+#          s(:call, s(:lvar, :a), :[], s(:arglist, s(:lvar, :i))),
+#          :+,
+#          s(:arglist,
+#           s(:call, s(:lvar, :b), :[], s(:arglist, s(:lvar, :i)))))))),
+#      s(:lasgn, :i, s(:call, s(:lvar, :i), :+, s(:arglist, s(:lit, 1))))),
+#     true),
+#    s(:return, s(:lvar, :prod)))))
+
+class Ruru
+  attr_reader :context
+
+  def initialize(code, context)
+    @context = context
+    if Sexp === code
+      @parsed = code
+    else
+      @parsed = RubyParser.new.parse(code)
+      # pp @parsed
+    end
+  end
+
+  def eval(sexp = @parsed)
+    type, *args = sexp
+    case type
+    when :defn
+      context.define_method(sexp[1], sexp[2].drop(1), sexp[3].dup)
+    when :scope
+      assert args.size == 1, ":scope with wrong args #{args.inspect}"
+      eval(args[0])
+    when :block
+      retval = nil
+      args.each do |arg|
+        retval = eval(arg)
+      end
+      retval
+    when :lit
+      RuFixnum.new(args[0])
+    when :lvar
+      context.get_variable(args[0])
+    when :lasgn
+      var, expr = args
+      val = eval(expr)
+      context.set_variable(var, val)
+    when :masgn
+      lhs, rhs = args
+      lhs = lhs.drop(1)
+      rhs = rhs.drop(1)
+      lhs.zip(rhs).each { |l,r| context.set_variable(l[1], eval(r)) }
+    when :while
+      cond, body = args
+      while eval(cond)
+        eval(body)
+      end
+      99
+    when :call
+      lhs, method, arglist = args
+      apply(lhs, method, arglist.drop(1))
+    when :return
+      eval(args[0])
+    else
+      raise "Parse error for #{sexp.inspect}"
+    end
+  end
+
+  def apply(obj, method, args)
+    obj = eval(obj)
+    args = args.map { |x| eval(x) }
+    return obj.apply(method, args)
+  end
+
+  def call(method, *args)
+    context.find_method(method).call(*args)
+  end
+end
+
+def ruru(code, context = RuContext.new)
+  ruru = Ruru.new(code, context)
+  ruru.eval
+  ruru
+end
+
+class RuContext
+  def initialize
+    @methods = {}
+    @variables = {}
+  end
+
+  def define_method(name, args, body)
+    @methods[name] = RuMethod.new(self, args, body)
+  end
+
+  def find_method(name)
+    @methods[name]
+  end
+
+  def set_variable(var, value)
+    @variables[var] = value
+  end
+
+  def get_variable(var)
+    @variables[var]
+  end
+end
+
+class RuMethod
+  def initialize(context, params, body)
+    @params = params
+    @body = body
+    @context = context
+  end
+
+  attr_reader :params, :body, :context
+
+  def call(*args)
+    params.zip(args).each do |param, arg|
+      context.set_variable(param, arg)
+    end
+    Ruru.new(body, context).eval
+  end
+end
+
+class RuFixnum
+  def initialize(val)
+    @val = val
+  end
+
+  def __val
+    @val
+  end
+
+  def apply(method, args)
+    arg = args[0]
+    case method
+    when :<
+      __val < arg.__val  # sb RuBoolean
+    when :+
+      RuFixnum.new(__val + arg.__val)
+    when :*
+      RuFixnum.new(__val * arg.__val)
+    else
+      raise "Method #{method}"
+    end
+  end
+end
+
+class RuArray
+  def initialize(arr)
+    @vals = arr
+  end
+
+  def apply(method, args)
+    arg = args[0]
+    case method
+    when :size
+      RuFixnum.new(@vals.size)
+    when :[]
+      i = args[0].__val
+      raise "Array bounds" if i < 0 || i >= @vals.size
+      @vals[i]
+    else
+      raise "Method #{method}"
+    end
+  end
+end
---
+++ spec/xprod_spec.rb
@@ -0,0 +1,39 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__),"..","interp")
+
+require 'ruru'
+
+describe "Cross Product calculator" do
+  attr_reader :r
+
+  before(:each) do
+    prog = <<-END
+      def xprod(a, b)
+        i, prod = 0, 0
+        while i < a.size
+          prod += a[i] * b[i]
+          i += 1
+        end
+        return prod
+      end
+    END
+    @r = ruru(prog)
+  end
+
+  def xprod(a, b)
+    a = a.map{ |x| RuFixnum.new(x) }
+    b = b.map{ |x| RuFixnum.new(x) }
+    r.call(:xprod, RuArray.new(a), RuArray.new(b)).__val
+  end
+
+  it "evaluates to 0 when the arrays are empty" do
+    xprod([], []).should == 0
+  end
+
+  it "adds up a when b is all 1" do
+    xprod([1,2,3,4,5], [1,1,1,1,1]).should == 15
+  end
+
+  it "throws an exception when array b is too short" do
+    lambda { xprod([1], []) }.should raise_error("Array bounds")
+  end
+end
